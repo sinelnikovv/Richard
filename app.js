94fa16607da1dfbe5244b3ec876b8c57de35747e
sinelnikovv <aktep010@gmail.com>
2022-09-04 21:49:36 +0300

    some changes with counters

--- app.js
+++ app.js
@@ -1,105 +1,76 @@
 //dark theme button
 
-let btn = document.getElementById('theme');
+let btn = document.getElementById("theme");
 
-btn.addEventListener('click', function() {
-    let body = document.querySelector('body');
-    body.classList.toggle('dark');
+btn.addEventListener("click", function () {
+  let body = document.querySelector("body");
+  body.classList.toggle("dark");
 });
 
 //splidejs
-var splide = new Splide('.splide', {
-  type: 'loop',
+var splide = new Splide(".splide", {
+  type: "loop",
   arrows: false,
-} );
+});
 splide.mount();
-
 
 //onview
 
 function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-    );
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
-const box = document.querySelector('#counters');
+//counters
 
-document.addEventListener('scroll', function () {
-  if (isInViewport(box)) {
-    let numbers = document.querySelectorAll('.number');
+const box = document.querySelector("#counters");
+let started = false;
 
-function interval(max, num, time) {
-  return setInterval(function () {
-    if (num.textContent < +max) {
-      num.textContent++;
-    } else {
-      clearInterval();
-    }
-  }, time
-  )
-}
+function counter() {
+  let numbers = document.querySelectorAll(".number");
 
-function numInterval(max, n, time) {
-  return setInterval(function () {
-    if (+n < +max) {
-      n++;
-      
-    } else {
-      clearInterval();
-    }
-  }, time
-  )
-}
-
-for (let num of numbers) {
-    
-  
-  if (num.textContent.includes(",")) {
-    
-    let numArr = num.textContent.split(",");
-    
-    
-    for (let n of numArr) {
-      
-      let max = n;
-      n = 0;
-      
-      if (max < 20) {
-        console.log(max);
-    numInterval(max, n,  400);
-    
-  } else if( max < 50) {
-    numInterval(max, n, 300);
-  } else if( max < 100) {
-    numInterval(max, n, 200);
-  }else {
-    numInterval(max, n, 100);
+  function interval(max, num, time) {
+    return setInterval(function () {
+      if (num.textContent < +max) {
+        num.textContent++;
+      } else {
+        clearInterval();
+      }
+    }, time);
   }
-    }
 
-  } else {
+  for (let num of numbers) {
     let max = num.textContent;
-  num.textContent = 0;
+    num.textContent = 0;
     if (max < 20) {
-    interval(max, num,  400);
-  } else if( max < 50) {
-    interval(max, num, 300);
-  } else if( max < 100) {
-    interval(max, num, 200);
-  }else {
-    interval(max, num, 100);
-  }}
-}
+      interval(max, num, 200);
+    } else if (max < 50) {
+      interval(max, num, 50);
+    } else if (max < 100) {
+      interval(max, num, 25);
+    } else {
+      interval(max, num, 25);
+    }
   }
-}, {
-    passive: true
-});
+}
+
+function watchCounter() {
+  if (isInViewport(box)) {
+    if (started == false) {
+      counter();
+      started = true;
+    }
+  } else {
+    started = false;
+  }
+}
+document.addEventListener("scroll", watchCounter);
 
 //fullpage scroll
 
@@ -112,16 +83,15 @@ function fullpageScroll(e) {
 
   if (delta > 0 && coords.top == 0) {
     window.scrollBy(0, window.innerHeight);
-  } else if(delta < 0 && coords.top == 0){    
+  } else if (delta < 0 && coords.top == 0) {
     window.scrollBy(0, -window.innerHeight);
   } else {
     window.scrollBy(0, coords.top);
   }
-
+  // this.removeEventListener("wheel", fullpageScroll, { once: true });
+  //  setTimeout(this.addEventListener("wheel", fullpageScroll, { once: true }), 1000);
 }
 
 for (let fullpage of fullpages) {
-  fullpage.addEventListener("wheel", fullpageScroll);
+  fullpage.addEventListener("scroll", fullpageScroll, { once: true });
 }
-
-
